4545d7859ef46e7e6eb8aebb7531dc1f86fc486c
Deborah <yhwh.dgse@gmail.com>
2025-09-21 23:52:39 +0300

    Add multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,8 @@
 # multiplication_table.py
-
+# Get a number from the user.
 number = int(input("Enter a number to see its multiplication table: "))
 
+# Use a for loop to iterate from 1 to 10.
 for i in range(1, 11):
-    print(number, "*", i, "=", number*i)
+    # Print the multiplication equation and its result.
+    print(number, "*", i, "=", number * i)
