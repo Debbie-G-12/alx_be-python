fb05278cd413d31814ee4a94c2f5ef34bdba767a
Deborah <yhwh.dgse@gmail.com>
2025-09-25 19:42:04 +0300

    Crate temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,1 +1,8 @@
+FACTOR = 9/5
+OFFSET = 32
 
+def celsius_to_fahrenheit(celsius):
+    return (celsius * FACTOR) + OFFSET
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - OFFSET) / FACTOR
